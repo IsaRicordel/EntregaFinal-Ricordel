0eb102a2f6dd29ea570f0ccca0b0797dc93c9553
Isabella Ricordel <isa.ricordel@gmail.com>
2023-07-23 03:55:52 -0300

    proyecto final con json integrado

--- js/script.js
+++ js/script.js
@@ -1,39 +1,35 @@
-// Tercera Pre-Entrega
-
-const productos = [
-    { id: 1, nombre: "Miura", marca: "La Sportiva", categoria: "Pédulas", precio: 85000, stock: 87, rutaImagen: "miura-lasportiva.jpg" },
-    { id: 5, nombre: "Katana", marca: "La Sportiva", categoria: "Pédulas", precio: 106000, stock: 150, rutaImagen: "katana-lasportiva.jpg" },
-    { id: 10, nombre: "Otaki", marca: "La Sportiva", categoria: "Pédulas",precio: 101000, stock: 237, rutaImagen: "otaki-lasportiva.jpg" },
-    { id: 15, nombre: "Selena", marca: "Petzl", categoria: "Arneses", precio: 42000, stock: 132, rutaImagen: "selena-petzl.jpeg" },
-    { id: 20, nombre: "Ray", marca: "Singing Rock", categoria: "Arneses", precio: 45500, stock: 103, rutaImagen: "ray-singingrock.jpg" },
-    { id: 25, nombre: "Jayne 3", marca: "Edelrid", categoria: "Arneses", precio: 63000, stock: 85, rutaImagen: "jayne3-edelrid.jpg" },
-    { id: 30, nombre: "Colt Negro", marca: "Singing Rock", categoria: "Mosquetones", precio: 7500, stock: 326, rutaImagen: "colt-singingrock.jpg" },
-    { id: 35, nombre: "Link", marca: "Edelweiss", categoria: "Mosquetones", precio: 9225, stock: 125, rutaImagen: "link-edelweiss.jpg" },
-    { id: 40, nombre: "Argon S Black", marca: "Kong", categoria: "Mosquetones", precio: 13450, stock: 238, rutaImagen: "kong-mosqueton.jpg" }
-]
+// Proyecto Final 
 
 let elCarrito = []
 let totalPrecio = 0
 let elCarritoJSON= JSON.parse(localStorage.getItem("elCarrito"))
 let totalPrecioJSON = localStorage.getItem("totalPrecio")
 
-
 if (elCarritoJSON) {
     elCarrito = elCarritoJSON
 }
 
-if (totalPrecioJSON) {
+if (totalPrecioJSON !== null) {
     totalPrecio = parseFloat(totalPrecioJSON)
 } else {
     totalPrecio = 0
 }
 
 let contenedor = document.getElementById("productos")
+const urlLocal =  "./db.json"
+let productos = []
 
-crearFiltros (productos)
-crearTarjetas(productos, contenedor)
-renderizarCarrito()
-
+fetch(urlLocal)
+.then(response => response.json())
+.then(data => {
+    productos = data.productos
+    crearFiltros (productos)
+    crearTarjetas(productos)
+    renderizarCarrito()
+})
+.catch(error => {
+    console.error("Error al obtener los productos:", error);
+});
 
 function crearTarjetas(array) {
     contenedor.innerHTML = ""
@@ -55,19 +51,21 @@ function crearTarjetas(array) {
 function agregarAlCarrito(e) {
     let productoBuscado = productos.find(elemento => elemento.id === Number(e.target.id))
     let productoEnCarrito = elCarrito.find(elemento => elemento.id === productoBuscado.id)
+    
     if (productoEnCarrito) {
         productoEnCarrito.cantidad = (productoEnCarrito.cantidad || 0) + 1
     } else {
     elCarrito.push({
         id: productoBuscado.id,
+        imagen: productoBuscado.rutaImagen,
         nombre: productoBuscado.nombre,
         precio: productoBuscado.precio,
         cantidad: 1
     })
 }
-
     totalPrecio += productoBuscado.precio
     console.log(elCarrito)
+    lanzarTostada()
     renderizarCarrito()
 
     localStorage.setItem("elCarrito", JSON.stringify(elCarrito))
@@ -79,11 +77,11 @@ function renderizarCarrito() {
     carritoFisico.innerHTML = ""
 
     elCarrito.forEach(elemento => {
+        let subtotal = elemento.cantidad * elemento.precio
+        subtotal = Math.round(subtotal)
         carritoFisico.innerHTML += `
-        <p>${elemento.nombre} (${elemento.cantidad}) ${elemento.precio.toString()}</p>
-        <button class="btn-restar" data-id="${elemento.id}">-</button>
-        <button class="btn-sumar" data-id="${elemento.id}">+</button>
-        <button class="eliminar" data-id="${elemento.id}">Eliminar</button>
+        <div class="carritoFinal"> 
+        <img class="imagenCarrito" src="../img/${elemento.imagen}"> ${elemento.nombre} <button class="btn-restar" data-id="${elemento.id}">-</button> ${elemento.cantidad} <button class="btn-sumar" data-id="${elemento.id}">+</button> <div class="carritoSubtotal"> Subtotal: $${subtotal.toFixed(0)}</div> <button class="eliminar" data-id="${elemento.id}">Eliminar</button>
         `
     })
 
@@ -91,8 +89,10 @@ function renderizarCarrito() {
     let cantidadCarrito = document.getElementById("cantidadCarrito")
     cantidadCarrito.textContent = cantidadItems
     
-    carritoFisico.innerHTML += `<p>Total items: ${cantidadItems}</p>`
-    carritoFisico.innerHTML += `<p>Total precio: $${totalPrecio}</p>`
+    carritoFisico.innerHTML += `<p class="totales">Total items: ${cantidadItems}</p>`
+    carritoFisico.innerHTML += `<p class="totales">Total precio: $${totalPrecio}</p>`
+    carritoFisico.innerHTML += `<button id="finalizar" class="compraFinalizada">Finalizar Compra</button>` /* nuevo */
+
 
     let botonesEliminar = document.getElementsByClassName("eliminar")
     for (const botonEliminar of botonesEliminar) {
@@ -107,7 +107,12 @@ function renderizarCarrito() {
     let botonesRestar = document.getElementsByClassName("btn-restar")
     for (const botonRestar of botonesRestar) {
         botonRestar.addEventListener("click", restarItem)
-    }    
+    }
+    
+    let botonesFinalizarCompra = document.getElementsByClassName("compraFinalizada") /* nuevo */
+    for (const botonFinalizarCompra of botonesFinalizarCompra) {
+        botonFinalizarCompra.addEventListener("click", finalizarCompra)
+    }
 }
 
 let buscador = document.getElementById("buscador")
@@ -210,5 +215,34 @@ function restarItem(e) {
     renderizarCarrito()
 }
 
+function lanzarTostada() {
+    Toastify({
+        text: "Producto agregado al carrito",
+        gravity: "bottom",
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },   
+        duration: 3000    
+    }).showToast();
+}
 
- 
+function finalizarCompra(e) { /*nuevo*/
+    let carritoFisico = document.getElementById("elCarrito")
+    carritoFisico.innerHTML = ""
+
+    if (e.target.id === "finalizar") {
+        Swal.fire({
+            icon: 'success',
+            title: '¡Compra Exitosa!',
+            text: '¡Muchas gracias por su Compra!',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    }
+
+    elCarrito = []
+    localStorage.clear()
+
+    let cantidadCarrito = document.getElementById("cantidadCarrito")
+    cantidadCarrito.textContent = 0
+}
